221e330a88d0e2c90bbb2373f4e75962c87c8980
Ryan Farrow <ryanfarrow@google.com>
2020-05-12 09:52:20 -0400

    Create project skeleton.

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,42 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {async, TestBed} from '@angular/core/testing';
+
+import {AppComponent} from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed
+        .configureTestingModule({
+          declarations: [AppComponent],
+        })
+        .compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'amp-story-ad-authoring-tool'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('amp-story-ad-authoring-tool');
+  });
+});
