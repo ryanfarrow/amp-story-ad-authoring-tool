304771d9a79dce56cb7aabf8905c701e9f05bef2
amakhdoom <amakhdoom@google.com>
2020-06-17 13:14:58 -0400

    removed some spacing and other unnecessary code

--- src/app/ad-authoring/ad-authoring.component.ts
+++ src/app/ad-authoring/ad-authoring.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AdAuthoringService } from './ad-authoring.service'
 import { AdAuthoring } from './ad-authoring';
 import { Observable } from 'rxjs';
@@ -12,7 +12,7 @@ interface Text {
   templateUrl: './ad-authoring.component.html',
   styleUrls: ['./ad-authoring.component.scss']
 })
-export class AdAuthoringComponent implements OnInit {
+export class AdAuthoringComponent {
 
   texts: Text[] = [
     {value: "Apply Now"},
@@ -46,9 +46,6 @@ export class AdAuthoringComponent implements OnInit {
 
   addAdAuthoring(landingUrl: string, landingType: string, callToAction: string) {
     this.service.addAdAuthoring(landingUrl, landingType, callToAction);
-  }
-
-  ngOnInit(): void {
   }
 
 }
