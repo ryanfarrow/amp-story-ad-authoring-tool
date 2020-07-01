8501f21112a90db1ccdc9ac1d18c1a6962a2560f
amakhdoom <amakhdoom@google.com>
2020-07-01 09:58:31 -0400

    added some service tests

--- src/app/ad-authoring/ad-authoring.service.spec.ts
+++ src/app/ad-authoring/ad-authoring.service.spec.ts
@@ -1,16 +1,41 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AdAuthoringService } from './ad-authoring.service';
+import { AdAuthoringWorkflowStateContainer } from './ad-authoring.state';
+import { LandingTypeEnum } from './landing-type-values';
+import { CallToActionEnum } from './call-to-action';
 
 describe('AdAuthoringService', () => {
   let service: AdAuthoringService;
+  let state: AdAuthoringWorkflowStateContainer;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(AdAuthoringService);
+    state = TestBed.inject(AdAuthoringWorkflowStateContainer)
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get state', () => {
+    const thisState = service.getAdAuthorings();
+    expect(thisState).toEqual(state.getState());
+  });
+
+  it('should update the landing page url state', () => {
+    service.updateLandingUrl('google.com');
+    expect(state.getValue().landingUrl).toBe('google.com');
+  });
+
+  it('should update the landing page type state', () => {
+    service.updateLandingType(LandingTypeEnum.AMP);
+    expect(state.getValue().landingType).toBe('AMP');
+  });
+
+  it('should update the call to action state', () => {
+    service.updateCallToAction(CallToActionEnum.ORDER_NOW);
+    expect(state.getValue().callToAction).toBe('ORDER_NOW');
   });
 });
