2bbc804ce110b0bcf92f55d5850dcb5cde439233
amakhdoom <amakhdoom@google.com>
2020-07-13 10:36:11 -0400

    some style changes

--- src/app/asset-upload/asset-upload.component.ts
+++ src/app/asset-upload/asset-upload.component.ts
@@ -19,21 +19,25 @@ export class AssetUploadComponent {
     this.adAuthoringObs = service.getAssets().pipe(tap(state => console.log(state)));
   }
   cardImageBase64: string = '';
+  file: File = null;
   onFileInput(fileInput: any) {
-    const file: File = fileInput.target.files[0];
-    console.log(file);
+    this.file = fileInput.target.files[0];
+    // console.log('logging the fileInput: ')
+    // console.log(fileInput);
     // save and update assets state
     // this.service.updateAssets(file);
+    console.log(this.file)
     const reader = new FileReader();
     reader.onload = (e: any) => {
-      const image = new Image();
-      image.src = e.target.result;
+      // console.log('logging e')
+      // console.log(e)
       const imgBase64Path = e.target.result;
       this.cardImageBase64 = imgBase64Path;
       this.service.updateAssets(this.cardImageBase64);
-      console.log('after updating assets in state');
+      // console.log('after updating assets in state');
+      // console.log(this.cardImageBase64);
     };
-    reader.readAsDataURL(fileInput.target.files[0]);
+    reader.readAsDataURL(this.file);
   }
 
 }
