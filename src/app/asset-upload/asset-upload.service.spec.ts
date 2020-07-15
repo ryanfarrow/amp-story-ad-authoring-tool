63f7fcb691f4fd13ca856f8e75574f0ca989ac2d
amakhdoom <amakhdoom@google.com>
2020-07-15 10:45:38 -0400

    added tests for asset upload service

--- src/app/asset-upload/asset-upload.service.spec.ts
+++ src/app/asset-upload/asset-upload.service.spec.ts
@@ -1,16 +1,37 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AssetUploadService } from './asset-upload.service';
+import { AdAuthoringWorkflowStateContainer } from '../ad-authoring/ad-authoring.state';
 
 describe('AssetUploadService', () => {
   let service: AssetUploadService;
+  let state: AdAuthoringWorkflowStateContainer;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(AssetUploadService);
+    state = TestBed.inject(AdAuthoringWorkflowStateContainer);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get state', () => {
+    const thisState = service.getAssets();
+
+    expect(thisState).toEqual(state.getState());
+  });
+
+  it('should update the assets state', () => {
+    let blob = new Blob([""], { type: 'image/png' });
+    blob["lastModifiedDate"] = "";
+    blob["name"] = "filename";
+    let fakeF = <File>blob;
+
+    service.updateAssets('image.png', fakeF);
+    
+    expect(state.getValue().fileSrc).toBe('image.png');
+    expect(state.getValue().file).toBe(fakeF);
   });
 });
