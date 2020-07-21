3e5665abcb03f377d6e592e0e268a9df8c0f5d5d
amakhdoom <amakhdoom@google.com>
2020-07-21 09:43:56 -0400

    added AMP ad html to the state and set a default value

--- src/app/ad-authoring/ad-authoring.state.ts
+++ src/app/ad-authoring/ad-authoring.state.ts
@@ -9,15 +9,37 @@ export interface AdAuthoringWorkflowState {
   readonly callToAction?: CallToActionEnum;
   readonly fileSrc?: string;
   readonly file?: File;
+  readonly AMPHTMLstring: string;
 }
-// add a default state for the fields above
+
 @Injectable({
   providedIn: 'root',
 })
 export class AdAuthoringWorkflowStateContainer {
+  AMPAdHTML = `<!doctype html>
+  <html ⚡4ads>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,minimum-scale=1">
+    <style amp4ads-boilerplate>body{visibility:hidden}</style>
+    <script async src="https://cdn.ampproject.org/amp4ads-v0.js"></script>
+  </head>
+  <body>
+    <p>Hello, fake ad with srcdoc.<p>
+    <amp-img
+      layout="fixed"
+      height="250"
+      width="300"
+      src="https://placekitten.com/300/250"
+    ></amp-img>
+  </body>
+  </html>`;
+
   private state$: BehaviorSubject<
     AdAuthoringWorkflowState
-  > = new BehaviorSubject({});
+  > = new BehaviorSubject({
+    AMPHTMLstring: this.AMPAdHTML,
+  });
 
   getValue(): AdAuthoringWorkflowState {
     return this.state$.getValue();
